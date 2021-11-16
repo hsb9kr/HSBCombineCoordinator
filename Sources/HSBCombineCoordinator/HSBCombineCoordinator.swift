863ce89dd802cad8f1f6e07174a5b6820ec75201
red <hsb9kr@gmail.com>
2021-11-16 18:53:06 +0900

    Initialize commit

--- Sources/HSBCombineCoordinator/HSBCombineCoordinator.swift
+++ Sources/HSBCombineCoordinator/HSBCombineCoordinator.swift
@@ -1,6 +1,30 @@
-public struct HSBCombineCoordinator {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import Combine
 
-    public init() {
-    }
+public open class BaseCoordinator<ResultType> {
+	private let identifier = UUID()
+	private var childCoordinators = [UUID: Any]()
+	public var cancellables: Set<AnyCancellable> = []
+	
+	private func store<T>(coordinator: BaseCoordinator<T>) {
+		childCoordinators[coordinator.identifier] = coordinator
+	}
+	
+	private func free<T>(coordinator: BaseCoordinator<T>) {
+		childCoordinators[coordinator.identifier] = nil
+	}
+	
+	public func coordinate<T>(to coordinator: BaseCoordinator<T>) -> AnyPublisher<T, Never> {
+		store(coordinator: coordinator)
+		return coordinator
+			.start()
+			.handleEvents(receiveOutput: { [weak self] _ in
+				self?.free(coordinator: coordinator)
+			})
+			.eraseToAnyPublisher()
+	}
+	
+	open func start() -> AnyPublisher<ResultType, Never> {
+		fatalError("Start method should be implemented.")
+	}
 }
